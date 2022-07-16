33a929da6a5725f6f5e6d1447d26baeb42288b08
예성 <yeseong31@naver.com>
2022-07-16 15:58:02 +0900

    0716 command & factory patterns

--- mvcStudent/src/student/CommandFactory.java
+++ mvcStudent/src/student/CommandFactory.java
@@ -2,12 +2,12 @@ package student;
 
 public class CommandFactory {
     // 1. private 생성자를 만든다.
-    private CommandFactory(){}
+    private CommandFactory() {}
 
     // 2. 내부에서 객체를 생성한다.
     private static final CommandFactory instance = new CommandFactory();
 
-    // 3. static으로 선언된 instance를 내부내는 메서드를 만든다.
+    // 3. static으로 선언된 instance를 내보내는 메서드를 만든다.
     public static CommandFactory getInstance() { return instance; }
 
     public CommandIf createCommand(String cmd) {
