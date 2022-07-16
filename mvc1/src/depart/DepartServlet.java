b3406acf62b7799e7efcdac04bca5549482e2767
예성 <yeseong31@naver.com>
2022-07-16 12:52:22 +0900

    0716 servlet 코드 수정

--- mvc1/src/depart/DepartServlet.java
+++ mvc1/src/depart/DepartServlet.java
@@ -11,19 +11,13 @@ public class DepartServlet extends HttpServlet {
 
     @Override
     protected void service(HttpServletRequest arg0, HttpServletResponse arg1) throws ServletException, IOException {
-        arg0.setCharacterEncoding("EUC-KR");
-        arg1.setContentType("text/html; charset=EUC-KR");
+        arg0.setCharacterEncoding("UTF-8");
+        arg1.setContentType("text/html; charset=UTF-8");
 
         String depart = arg0.getParameter("depart");
 
         DepartExpert de = new DepartExpert();
         List<String> list = de.advice(depart);
-
-//		PrintWriter pw = arg1.getWriter();
-//		for (String msg : list) {
-//			pw.println("<h2>" + msg + "</h2>");
-//		}
-//		pw.close();
 
         // request를 JSP로 전달
         arg0.setAttribute("advice", list);
