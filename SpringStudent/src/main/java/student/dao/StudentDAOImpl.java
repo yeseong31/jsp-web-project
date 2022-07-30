fe1e51118cb88dcdd44c7e6e8fbab3f4a06701dc
예성 <yeseong31@naver.com>
2022-07-30 14:46:26 +0900

    0730

--- SpringStudent/src/main/java/student/dao/StudentDAOImpl.java
+++ SpringStudent/src/main/java/student/dao/StudentDAOImpl.java
@@ -5,9 +5,11 @@ import java.sql.SQLException;
 import java.util.List;
 
 import student.dto.StudentDTO;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 
 public class StudentDAOImpl implements StudentDAO {
-	
+
 	private JdbcTemplate jdbcTemplate;
 	public void setJdbcTemplate(JdbcTemplate jdbcTemplate) {
 		this.jdbcTemplate = jdbcTemplate;
@@ -17,35 +19,31 @@ public class StudentDAOImpl implements StudentDAO {
 	public int insertStudent(StudentDTO dto) {
 		String sql = "insert into student values(?, ?, ?)";
 		Object[] values = new Object[] {dto.getId(), dto.getName(), dto.getCname()};
-		int res = jdbcTemplate.update(sql, values);
-		return res;
+		return jdbcTemplate.update(sql, values);
 	}
 
 	@Override
 	public int deleteStudent(String id) {
 		String sql = "delete from student where id = ?";
-		int res = jdbcTemplate.update(sql, id);
-		return res;
+		return jdbcTemplate.update(sql, id);
 	}
 
 	@Override
 	public List<StudentDTO> findStudent(String name) {
 		String sql = "select * from student where name = ?";
 		MyRowMapper mapper = new MyRowMapper();
-		List<StudentDTO> list = jdbcTemplate.query(sql, mapper, name);
-		return list;
+		return jdbcTemplate.query(sql, mapper, name);
 	}
 
 	@Override
 	public List<StudentDTO> listStudent() {
 		String sql = "select * from student";
 		MyRowMapper mapper = new MyRowMapper();
-		List<StudentDTO> list = jdbcTemplate.query(sql, mapper);
-		return list;
+		return jdbcTemplate.query(sql, mapper);
 	}
 	
 	// 내부 클래스
-	class MyRowMapper implements RowMapper<StudentDTO> {
+	static class MyRowMapper implements RowMapper<StudentDTO> {
 		@Override
 		public StudentDTO mapRow(ResultSet rs, int arg1) throws SQLException {
 			StudentDTO dto = new StudentDTO();
